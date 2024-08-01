ce84f8b80a0f87a4a14cf81051a676a437021cde
mmr2810 <69448469+mmr2810@users.noreply.github.com>
2024-08-02 02:33:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0]);
-  e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+  if (e.target.matches('.next')) {
+    slider.append(items[0]);
+  } else if (e.target.matches('.prev')) {
+    slider.prepend(items[items.length - 1]);
+  }
 }
-document.addEventListener('click',activate,false);
+
+document.addEventListener('click', activate, false);
